7f51928fb15aea62b7cbc58a8db818c099e4dfdc
Rika4698 <rikasharmin181@gmail.com>
2023-11-06 16:08:13 +0600

    create error messsage without navbar and footer

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -3,12 +3,14 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Registration from "../Pages/Registration/Registration";
+import ErrorMessage from "../Pages/ErrorMessage/ErrorMessage";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement:<ErrorMessage></ErrorMessage>,
       children:[
         {
             path:'/',
