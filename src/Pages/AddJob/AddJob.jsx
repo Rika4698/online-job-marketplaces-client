52afcd5f7a39926d106c376867542739add97a0a
Rika4698 <rikasharmin181@gmail.com>
2023-11-07 13:31:43 +0600

    give data in add job and send this server side

--- src/Pages/AddJob/AddJob.jsx
+++ src/Pages/AddJob/AddJob.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Hook/AuthProvider";
+import swal from "sweetalert";
 
 
 const AddJob = () => {
@@ -25,6 +26,27 @@ const AddJob = () => {
         const newJob = { name, image, email,deadline, category, description,minPrice,maxPrice }
 
         console.log(newJob);
+        //send data to the server
+        fetch('http://localhost:5000/jobs',{
+            method:'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(newJob)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId)
+            {
+            swal({
+                title: 'okay!',
+                text: 'Job Added Successfully ',
+                icon:'success',
+            })
+            }
+
+        })
 
     }
     return (
@@ -67,26 +89,7 @@ const AddJob = () => {
                             <span className="label-text font-semibold text-base">Deadline:</span>
                         </label>
                         <label className="input-group">
-                        <input type="text" placeholder="Deadline" className="input input-bordered w-full"  name="deadline" />
-                        </label>
-                    </div>
-                </div>
-            
-                <div className="md:flex mb-8">
-                    <div className="form-control md:w-1/2">
-                        <label className="label">
-                            <span className="label-text font-semibold text-base">Description:</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="description" placeholder="Description" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                    <div className="form-control md:w-1/2 ml-4">
-                        <label className="label">
-                            <span className="label-text font-semibold text-base">Category:</span>
-                        </label>
-                        <label className="input-group">
-                            <input type="text" name="category" placeholder="Category" className="input input-bordered w-full" />
+                        <input type="date" placeholder="Deadline" className="input input-bordered w-full"  name="deadline" />
                         </label>
                     </div>
                 </div>
@@ -101,8 +104,7 @@ const AddJob = () => {
                     </div>
                     <div className="form-control md:w-1/2 ml-4">
                         <label className="label">
-                            <span className="label-text font-semibold text-base">Maximum Price
-:</span>
+                            <span className="label-text font-semibold text-base">Maximum Price:</span>
                         </label>
                         <label className="input-group">
                             <input type="text" name="maxPrice" placeholder="Max-Price" className="input input-bordered w-full" />
@@ -110,8 +112,39 @@ const AddJob = () => {
                     </div>
                 </div>
             
+                <div className="md:flex mb-8">
+                   
+                    <div className="form-control md:w-1/2 ">
+                        <label className="label">
+                            <span className="label-text font-semibold text-base">Category:</span>
+                        </label>
+                        <label className="input-group">
+                        {/* <input type="text" name="category" placeholder="Category" className="input input-bordered w-full" /> */}
+                        <select name="category" className="select select-bordered join-item w-full "  >
+      <option className="text-slate-400"  disabled selected>Select</option>
+      <option >Web Development</option>
+      <option>Digital Marketing</option>
+      <option>Graphics Design</option>
+    </select>
+                            
+                        </label>
+                    </div>
+                    <div className="form-control md:w-1/2 ml-4">
+                        <label className="label">
+                            <span className="label-text font-semibold text-base">Description:</span>
+                        </label>
+                        <label className="input-group">
+                        <textarea type="text" name="description"  placeholder="Description" className="textarea textarea-bordered textarea-lg w-full max-w-full" ></textarea>
+                            {/* <input type="text" name="description" placeholder="Description" className="input input-bordered w-full h-[80px]" /> */}
+                        </label>
+                    </div>
+                </div>
+               
+
+               
+            
                 
-                <div className="text-center text-xl rounded-lg bg-lime-500 w-40 h-8 text-white lg:ml-60 lg:w-96 xl:ml-96">
+                <div className="text-center  text-xl rounded-lg bg-lime-500 w-40 h-8 text-white lg:ml-60 lg:w-96 xl:ml-96">
                 <button >Add Button</button>
                 </div>
 
