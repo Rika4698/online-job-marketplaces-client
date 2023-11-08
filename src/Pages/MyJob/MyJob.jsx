7a07f8a5b76e2456e6ab4b8f4ab0aa90d29ac1ca
Rika4698 <rikasharmin181@gmail.com>
2023-11-08 08:11:54 +0600

    create handle delete using delete method and delete job

--- src/Pages/MyJob/MyJob.jsx
+++ src/Pages/MyJob/MyJob.jsx
@@ -1,16 +1,21 @@
+// import { useContext } from "react";
 import { Link } from "react-router-dom";
+// import { AuthContext } from "../../Hook/AuthProvider";
+// import Swal from "sweetalert2";
 
 
-const MyJob = ({cards}) => {
+const MyJob = ({cards,handleDelete}) => {
+    // const { user } = useContext(AuthContext);
     const {_id, name, image, deadline, category, description,minPrice,maxPrice } = cards || {};
     const paragraphs = description.split('\n');
 
     // console.log(description.split('\n'));
+   
     return (
         <div>
             <div className="card my-4 lg:card-side lg:mx-10 lg:my-4 bg-base-100 shadow">
   <figure className=" w-auto lg:rounded-none  lg:w-[800px] lg:h-[250px]"><img src={image} alt=""/></figure>
-  <div className="card-body">
+  <div className="card-body bg-teal-50">
     <h2 className="card-title font-bold text-3xl">{name}</h2>    
     <h3 className="text-gray-400">Category: {category}</h3>    
     <h3 className="text-black">Deadline:  {deadline}</h3>    
@@ -25,7 +30,7 @@ const MyJob = ({cards}) => {
     <Link to={`/update/${_id}`}>
       <button className="btn capitalize text-lg font-bold text-white bg-pink-400">Update</button>
       </Link>
-      <button className="btn capitalize text-lg font-bold text-white bg-red-500">Delete</button>
+      <button onClick={()=>handleDelete(cards._id)} className="btn capitalize text-lg font-bold text-white bg-red-500">Delete</button>
     </div>
   </div>
 </div>
--- src/Pages/MyJob/MyJobSet.jsx
+++ src/Pages/MyJob/MyJobSet.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Hook/AuthProvider";
 import MyJob from "./MyJob";
+import Swal from "sweetalert2";
 
 
 const MyJobSet = () => {
@@ -23,6 +24,51 @@ const MyJobSet = () => {
        
     },[email,job])
     // console.log(jobCard.length);
+    const handleDelete = (id) => {
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            console.log(result);
+            const info = { email: user.email, id: id }
+            console.log(info);
+            if (result.isConfirmed) {
+                fetch("http://localhost:5000/jobs", {
+                    method: 'DELETE',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(info)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        const filtered = jobCard.filter(cart => cart._id != id && user.email == cart.email);
+                        console.log(filtered);
+                        setJobCard(filtered);
+                        console.log(data);
+                        if ((data.deletedCount >= 0)) {
+                            Swal.fire({
+                                position: 'top-end',
+                                icon: 'success',
+                                title: 'Delete job succesfully.',
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                            // setTotalLength(totalLength - 1);
+                        }
+                      
+                    }
+                    )
+                    
+            }
+        })
+    };
     return (
         <div>
              {
@@ -35,7 +81,7 @@ const MyJobSet = () => {
             <div >
             {/* className="grid grid-cols-1 ml-16 pb-12 pt-20   md:grid-cols-2  lg:grid-cols-3  mr-6 gap-8"> */}
             {
-            jobCard.map(cards => <MyJob key={cards._id} cards={cards}></MyJob>
+            jobCard.map(cards => <MyJob key={cards._id} cards={cards} handleDelete={handleDelete}></MyJob>
                 )
             }
              </div>
